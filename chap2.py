4d99c3110fbb78a75a195002e45c8d6036d5c062
Junhui Zhang <junhuizhang42@gmail.com>
2025-02-27 15:11:13 +0800

    using PyTorch’s DataLoader class to generate mini-batches out of a dataset

--- chap2.py
+++ chap2.py
@@ -75,6 +75,15 @@ train_data = CustomDataset(x_train_tensor, y_train_tensor)
 
 print("train_data[0]:\n", train_data[0])
 
+train_loader = DataLoader(
+	dataset=train_data, 
+	batch_size=16, 
+	shuffle=True)
+
+
+
+
+
 '''Model Configuration'''
 
 # Step 0 - Initializes parameters "b" and "w" randomly
@@ -103,7 +112,18 @@ losses = []
 
 for epoch in range(n_epochs):
 
-	loss = train_step_fn(x_train_tensor, y_train_tensor)
+	mini_batch_losses = []
+
+	for x_batch, y_batch in train_loader:
+
+		x_batch = x_batch.to(device)
+		y_batch = y_batch.to(device)
+
+		mini_batch_loss = train_step_fn(x_batch, y_batch)
+		mini_batch_losses.append(mini_batch_loss)
+
+	loss = np.mean(mini_batch_losses)
 	losses.append(loss)
+
 
 
